1c7b829f4d5f12e14304968dda2b082506017eac
Tien Dung <69684422+tiendung14b@users.noreply.github.com>
2023-05-14 21:12:56 +0700

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,231 @@
+const ai = {
+  role: 'o',
+  ene: 'x',
+  board: [],
+  scoreBoard: [],
+  cntMove: 0,
+  maxMove: 30,
+  minMove: 10,
+  
+  checkWin: function (board, pos, roleCheck) {
+    if (countRow(board, pos, roleCheck) >= 5 ||
+        countCol(board, pos, roleCheck) >= 5 ||
+        countDownDiag(board, pos, roleCheck) >= 5 ||
+        countUpDiag(board, pos, roleCheck) >= 5) {
+      return 1;
+    }
+    return 0;
+  },
+
+  checkLost: function (board, pos, roleCheck) {
+    if (countRow(board, pos, roleCheck) >= 4 ||
+      countCol(board, pos, roleCheck) >= 4 ||
+      countDownDiag(board, pos, roleCheck) >= 4 ||
+      countUpDiag(board, pos, roleCheck) >= 4) {
+      return 1;
+    }
+    return 0;
+  },
+
+  setScoreBoard: function () {
+    // init score board
+    for (let i = 0; i < this.board.length; i++) {
+      this.scoreBoard[i] = [];
+      for (let j = 0; j < this.board[0].length; j++) {
+        if (this.board[i][j] === ' ') {
+          this.scoreBoard[i][j] = 0;
+        } else {
+          this.scoreBoard[i][j] = -1;
+        }
+      }
+    }
+    // set score board
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[0].length; j++) {
+        if (this.board[i][j] === ' ') {
+          this.board[i][j] = this.role;
+          this.scoreBoard[i][j] = Math.max(countRow(this.board, [i, j], this.role),
+                                      countCol(this.board, [i, j], this.role),
+                                      countDownDiag(this.board, [i, j], this.role),
+                                      countUpDiag(this.board, [i, j], this.role));
+          this.board[i][j] = this.ene;
+          this.scoreBoard[i][j] = Math.max(this.scoreBoard[i][j],
+                                      countRow(this.board, [i, j], this.ene),
+                                      countCol(this.board, [i, j], this.ene),
+                                      countDownDiag(this.board, [i, j], this.ene),
+                                      countUpDiag(this.board, [i, j], this.ene));
+          this.board[i][j] = ' ';
+        }
+      }
+    }
+  },
+
+  getChildNodes: function () {
+    this.setScoreBoard();
+    let availableNodes = [];
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[0].length; j++) {
+        if (this.board[i][j] === ' ') {
+          availableNodes.push([i, j]);
+        }
+      }
+    }
+    availableNodes.sort((a, b) => {
+      return this.scoreBoard[b[0]][b[1]] - this.scoreBoard[a[0]][a[1]]; 
+    });
+    let childNodes = [];
+    for (let i = 0; i < this.maxMove; i++) {
+      if (availableNodes[i] !== undefined && this.scoreBoard[availableNodes[i][0]][availableNodes[i][1]] === 0) {
+        break;
+      }
+      childNodes.push(availableNodes[i]);
+    }
+    if (childNodes.length === 0) {
+      for (let i = 0; i < this.minMove; i++) {
+        if (availableNodes[i] === undefined) {
+          break;
+        }
+        childNodes.push(availableNodes[i]);
+      }
+    }
+    return childNodes;
+  },
+
+
+  evaluate: function (pos, board, alpha, beta, depth, roleCheck) {
+    if (pos[0] == 0 && pos[0] == board.length - 1 && pos[1] == 0 && pos[1] == board[0].length - 1 ) {
+      if (this.checkWin(board, pos, this.role)) {
+        return 1000000;
+      } else {
+        return -1000000;
+      }
+    }
+    if (depth == 5) {
+      if (this.checkWin(board, pos, this.role)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+    let childNodes = this.getChildNodes();
+    if (roleCheck === this.role) {
+      let maxScore = -Infinity;
+      for (let i = 0; i < childNodes.length; i++) {
+        board[childNodes[i][0]][childNodes[i][1]] = this.role;
+        if (this.checkWin(board, childNodes[i], this.role)) {
+          board[childNodes[i][0]][childNodes[i][1]] = ' ';
+          return 1000000;
+        }
+        let score = this.evaluate(childNodes[i], board, alpha, beta, depth + 1, this.ene);
+        board[childNodes[i][0]][childNodes[i][1]] = ' ';
+        maxScore = Math.max(maxScore, score);
+        alpha = Math.max(alpha, score);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+      return maxScore;
+    }
+    if (roleCheck === this.ene) {
+      let minScore = Infinity;
+      for (let i = 0; i < childNodes.length; i++) {
+        board[childNodes[i][0]][childNodes[i][1]] = this.ene;
+        if (this.checkLost(board, childNodes[i], this.ene)) {
+          board[childNodes[i][0]][childNodes[i][1]] = ' ';
+          return -1000000;
+        }
+        let score = this.evaluate(childNodes[i], board, alpha, beta, depth + 1, this.role);
+        board[childNodes[i][0]][childNodes[i][1]] = ' ';
+        minScore = Math.min(minScore, score);
+        beta = Math.min(beta, score);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+      return minScore;
+    }
+  },
+
+
+  random: function () {
+    let randoms = [];
+    for (let i = parseInt(this.board.length / 2) - 1; i <= parseInt(this.board.length / 2) + 1; i++) {
+      for (let j = parseInt(this.board[0].length / 2) - 1; j <= parseInt(this.board[0].length / 2) + 1; j++) {
+        if (this.board[i][j] === ' ') {
+          randoms.push([i, j]);
+        }
+        console.log(i, j)
+      }
+    }
+    return randoms[Math.floor(Math.random() * randoms.length)];
+  },
+
+  findFinalMove: function () {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[0].length; j++) {
+        if (this.board[i][j] === ' ') {
+          this.board[i][j] = this.role;
+          if (this.checkWin(this.board, [i, j], this.role)) {
+            return [i, j];
+          }
+          this.board[i][j] = ' ';
+        }
+      }
+    }
+    return false;
+  },
+
+  findEnemyFinalMove: function () {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[0].length; j++) {
+        if (this.board[i][j] === ' ') {
+          this.board[i][j] = this.ene;
+          if (this.checkWin(this.board, [i, j], this.ene)) {
+            return [i, j];
+          }
+          this.board[i][j] = ' ';
+        }
+      }
+    }
+    return false;
+  },
+
+  move: function (board, role) {
+    this.cntMove++;
+    this.board = board;
+    this.role = role;
+    this.ene = role === 'x' ? 'o' : 'x';
+
+    if (this.cntMove < 3) {
+      return this.random();
+    }
+    
+    let finalMove = this.findFinalMove();
+    let enemyFinalMove = this.findEnemyFinalMove();
+    if (finalMove) {
+      return finalMove;
+    } else if (enemyFinalMove) {
+      return enemyFinalMove;
+    }
+
+    let childNodes = this.getChildNodes();
+    console.log("childNodes", childNodes);
+    let bestScore = -Infinity;
+    let bestMove = [];
+
+    for (let i = 0; i < childNodes.length; i++) {
+      this.board[childNodes[i][0]][childNodes[i][1]] = this.role;
+      let score = this.evaluate(childNodes[i], this.board, -Infinity, Infinity, 0, this.ene);
+      this.board[childNodes[i][0]][childNodes[i][1]] = ' ';
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove.push([childNodes[i], score]);
+      }
+    }
+    // sort bestMove
+    bestMove = bestMove.filter((move) => {
+      return move[1] === bestScore;
+    });
+    return bestMove[Math.floor(Math.random() * bestMove.length)][0];
+  }
+}
---
+++ game.js
@@ -0,0 +1,180 @@
+// board
+const boardHtml = document.querySelector('.board');
+const cols = 15;
+const rows = 20;
+const map = {};
+let prevx = [0, 0];
+let prevo = [0, 0];
+let gameOver = false;
+map['<i class="fa-solid fa-xmark"></i>'] = 'x';
+map['<i class="fa-solid fa-o"></i>'] = 'o';
+map['x'] = '<i class="fa-solid fa-xmark"></i>';
+map['o'] = '<i class="fa-solid fa-o"></i>';
+
+// convert boardhtml to board
+
+// play
+const role = 'x';
+const aiRole = 'o';
+
+const boxs = Array.from(document.querySelectorAll('.box'));
+boxs.forEach(box => {
+  box.addEventListener('mouseenter', () => {
+    if (!box.classList.contains('box--active')) {
+      box.classList.add('box--hover');
+      box.innerHTML = `<i class="fa-solid fa-xmark fa-fade"></i>`;
+    }
+  });
+  box.addEventListener('mouseleave', () => {
+    box.classList.remove('box--hover')
+    if (!box.classList.contains('box--active')) {
+      box.innerHTML = ' ';
+    }
+  });
+})
+
+let getBoard = function () {
+  const board = [];
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i][j] = ' ';
+    }
+  }
+  let cnt = 0;
+  boxs.forEach(box => {
+    if (box.classList.contains('box--active')) {
+      board[Math.floor(cnt / cols)][cnt % cols] = map[box.innerHTML];
+    }
+    cnt++;
+  })
+  return board;
+}
+
+const updateBox = (box, currRole) => {
+  if (!box.classList.contains('box--active')) {
+    box.classList.add('box--active', 'box--active' + currRole);
+    box.innerHTML = map[currRole];
+  }
+}
+
+// utility funtion
+
+const countRow = (board, pos, roleCheck) => {
+  let bottomRow = 0;
+  let topRow = 0;
+  let i = pos[0] + 1;
+  let j = pos[1];
+  while (i < rows && board[i][j] === roleCheck) {
+    bottomRow++;
+    i++;
+  }
+  i = pos[0] - 1;
+  while (i >= 0 && board[i][j] === roleCheck) {
+    topRow++;
+    i--;
+  }
+  return bottomRow + topRow + 1;
+}
+
+const countCol = (board, pos, roleCheck) => {
+  let leftCol = 0;
+  let rightCol = 0;
+  let i = pos[0];
+  let j = pos[1] + 1;
+  while (j < cols && board[i][j] === roleCheck) {
+    rightCol++;
+    j++;
+  }
+  j = pos[1] - 1;
+  while (j >= 0 && board[i][j] === roleCheck) {
+    leftCol++;
+    j--;
+  }
+  return leftCol + rightCol + 1;
+}
+
+const countDownDiag = (board, pos, roleCheck) => {
+  let leftDiag = 0;
+  let rightDiag = 0;
+  let i = pos[0] + 1;
+  let j = pos[1] + 1;
+  while (i < rows && j < cols && board[i][j] === roleCheck) {
+    rightDiag++;
+    i++;
+    j++;
+  }
+  i = pos[0] - 1;
+  j = pos[1] - 1;
+  while (i >= 0 && j >= 0 && board[i][j] === roleCheck) {
+    leftDiag++;
+    i--;
+    j--;
+  }
+  return leftDiag + rightDiag + 1;
+}
+
+const countUpDiag = (board, pos, roleCheck) => {
+  let leftDiag = 0;
+  let rightDiag = 0;
+  let i = pos[0] - 1;
+  let j = pos[1] + 1;
+  while (i >= 0 && j < cols && board[i][j] === roleCheck) {
+    rightDiag++;
+    i--;
+    j++;
+  }
+  i = pos[0] + 1;
+  j = pos[1] - 1;
+  while (i < rows && j >= 0 && board[i][j] === roleCheck) {
+    leftDiag++;
+    i++;
+    j--;
+  }
+  return leftDiag + rightDiag + 1;
+}
+
+const getPos = (box) => {
+  return [parseInt(boxs.indexOf(box) / cols), boxs.indexOf(box) % cols];
+}
+
+const getBox = (pos) => {
+  return boxs[pos[0] * cols + pos[1]];
+}
+
+const checkWin = (board, pos, roleCheck) => {
+  return countRow(board, pos, roleCheck) >= 5 ||
+         countCol(board, pos, roleCheck) >= 5 ||
+         countDownDiag(board, pos, roleCheck) >= 5 ||
+         countUpDiag(board, pos, roleCheck) >= 5;
+}
+
+// playing game
+
+boxs.forEach(box => {
+  box.addEventListener('click', () => {
+    if (box.classList.contains('box--active') || gameOver) {
+      return;
+    }
+    updateBox(box, role);
+    if (checkWin(getBoard(), getPos(box), role)) {
+      document.querySelector('.winner').innerHTML = 'Winner: ' + map[role];
+      gameOver = true;
+      return;
+    }
+    document.querySelector('.turn').innerHTML = 'Turn: ' + map['o'];
+    let move = ai.move(getBoard(), aiRole);
+    if (prevo !== undefined && getBox(prevo).classList.contains('box--prevo')) {
+      getBox(prevo).classList.remove('box--prevo');
+    }
+    prevo = move;
+    getBox(move).classList.add('box--prevo');
+    updateBox(boxs[move[0] * cols + move[1]], aiRole);
+    if (checkWin(getBoard(), move, aiRole)) {
+      document.querySelector('.winner').innerHTML = 'Winner: ' + map[aiRole];
+      gameOver = true;
+      return;
+    }
+    document.querySelector('.turn').innerHTML = 'Turn: ' + map['x'];
+  });
+})
